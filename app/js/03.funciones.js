a2fe67a82562a8827b4eade19ffd8ea9b84f2ebd
David Martín Pérez <davidmartinperez1@gmail.com>
2018-05-24 22:35:52 +0200

    remove js

--- app/js/03.funciones.js
+++
@@ -1,7 +0,0 @@
-// Funciones y tipado fuerte
-// En las funciones se pueden definir los valor que se van a recibir
-// Y las que va a devolver.
-//(valor:tipoDatoRecibido):tipoDatoDevolver => { return dato }
-function obtener(numero) {
-    return "";
-}
--- app/js/05.interfaces.js
+++
@@ -1,4 +0,0 @@
-// Clase( recipiente del objecto )
-// class Coche implements CocheBase{
-var ford = new Coche("Kuga", "Blanco", "57275BMX", 2017);
-console.log(ford);
